71617863fcd8154eb34a3c4e7b4b77767095a09f
liuxu <liuxu@vtstar.net>
2019-03-14 17:10:05 +0800

    update

--- src/main/java/net/vtstar/codegenerator/generate/file/properties/GeneratorProperties.java
+++ src/main/java/net/vtstar/codegenerator/generate/file/properties/GeneratorProperties.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Profile;
 
 
 @Profile("pro")
-@ConfigurationProperties(prefix = "genenrator")
+@ConfigurationProperties(prefix = "generator")
 public class GeneratorProperties {
 
     @Getter
